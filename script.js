e5ea9d8ffc1c8d0e163bb7c7ab923f764eb9aba6
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2025-02-03 10:30:34 +0200

    todo list added

---
+++ script.js
@@ -0,0 +1,6 @@
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+const clearAllTask = document.getElementById('clearAllTask');
+const displayAlert = document.getElementById('displayAlert');
+addButton.addEventListener('click',addTask);
